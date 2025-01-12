9c19e8162134678695d375be5145896152cef3ec
Toyyib <hamzatbolaji648@yahoo.com>
2025-01-12 01:18:04 +0100

    jwt

--- api/main.go
+++ api/main.go
@@ -17,7 +17,32 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 func isAuthorized(endpoint func(http.ResponseWriter, *http.Request)) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Header["Token"] != nil {
+			jwt.Parse(r.Header["Token"][0], func(token *jwt.Token) (interface{}, error) {
+				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+					return nil, fmt.Errorf("Invalid signing error")
+				}
+				aud := "billing.jwtgo.io"
+				checkAudience := token.Claims.(jwt.MapClaims).VerifyAudience(aud, false)
+				if !checkAudience {
+					return nil, fmt.Errorf("Invalid audience")
+				}
+				iss := "jwtgo.io"
+				checkIss := token.Claims.(jwt.MapClaims).VerifyIssuer(iss, false)
+				if !checkIss {
+					return nil, fmt.Errorf("Invalid issuer")
+				}
+				return MySigningKey, nil
+			})
+			//if err != nil {
+			//	fmt.Printf(w, err.Error())
+			//}
 
+			if token.Valid {
+				endpoint(w, r)
+			}
+
+		} else {
+			fmt.Fprintf(w, "No authorization token provided")
 		}
 	})
 }
